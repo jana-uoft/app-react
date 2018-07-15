c046e6c5ad1ba60d6b4e01ae9cfc9ebf2b32870a
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-15 00:36:01 -0400

    Test failing test in jenkins pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  return Promise.reject()
+  expect(true).toBe(false);
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
