5316a046f0be3a3e14f01410d5f0d7ae3a4f6076
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-15 00:33:21 -0400

    Always post test results

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  return false
+  return Promise.reject()
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
